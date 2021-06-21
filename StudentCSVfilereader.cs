a06d64fd5c99c527d7bd494a8a558a793d15043e
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-06-21 20:16:27 +0530

    Add files via upload

---
+++ StudentCSVfilereader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Demo
+{
+    class StudentCSVfilereader
+    {
+        
+        public void validatefilepath()
+        {
+            string filepath = (@"D:\visual studio\info.csv");
+            if (File.Exists(filepath))
+            {
+                Console.WriteLine("File Exists");
+            }
+            else
+            {
+                Console.WriteLine("file does not exists");
+            }
+
+        }
+        public void GetALLStudents()
+        {
+            string a = File.ReadAllLines(@"D:\visual studio\info.csv");
+
+           
+
+
+        }
+    }
+}
