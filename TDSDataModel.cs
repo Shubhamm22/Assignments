3605d8b3bb403d4cf64cfaf8c52fa4964923e526
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-06-22 15:41:53 +0530

    Add files via upload

---
+++ TDSDataModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskCalculator
+{
+    class TDSDataModel
+    {
+        private string tradeid;
+        private string date;
+        private string currenttradevalue;
+        private string counterpartyid;
+        public string gettradeid() { return this.tradeid; }
+        public string getdate() { return this.date; }
+        public string getcurrenttradevalue() { return this.currenttradevalue; }
+        public string getcounterpartyid() { return this.counterpartyid; }
+        public void settradeid(string value) { this.tradeid = value; }
+        public void setdate(string value) { this.date = value; }
+        public void setcurrenttradevalue(string value) { this.currenttradevalue = value; }
+        public void setcounterpartyid(string value) { this.counterpartyid = value; }
+
+
+    }
+}
---
+++ calculaterisk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskCalculator
+{
+    class calculaterisk
+    {
+        private void calculate()
+        {
+
+        }
+    }
+}
